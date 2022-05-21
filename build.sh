8f02e58eb77a1febd4f54ce8b20e1371977d84e7
Colin Davidson <colrdavidson@gmail.com>
2022-05-20 18:27:59 -0700

    Add gross little tool to convert from hextxt -> binary

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm -rf build
 mkdir -p build
-xxd -r -p tests/bintest.txt build/bintest.bin
 gcc src/main.c src/disx86.c -g -o build/disx86
+gcc src/hexbin.c -g -o build/hexbin
+./build/hexbin tests/bintest.txt build/bintest.bin
