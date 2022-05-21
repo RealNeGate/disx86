b6f3d13ae0f11ef9a52b514877d62cec18f7c58d
Colin Davidson <colrdavidson@gmail.com>
2022-05-21 16:57:54 -0700

    add kit building to linux build

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 rm -rf build
-mkdir -p build
-gcc src/main.c src/disx86.c -g -o build/disx86
+
+mkdir build
+
+DISKIT=build/disx86
+mkdir $DISKIT
+mkdir $DISKIT/lib
+mkdir $DISKIT/include
+
+gcc -c -fPIC src/disx86.c -g -o build/disx86.o
+ar rcs $DISKIT/lib/libdisx86.a build/disx86.o
+cp src/disx86.h $DISKIT/include/.
+cp src/public.inc $DISKIT/include/.
+echo 'library kit @ '$(echo ./$DISKIT/)
+
+gcc src/main.c $DISKIT/lib/libdisx86.a -g -o build/dis
 gcc src/hexbin.c -g -o build/hexbin
 ./build/hexbin tests/bintest.txt build/bintest.bin
